072ef649ca670aa38b6cded8363400ad95d83c7f
Krin Boven <krinboven17@gmail.com>
2024-12-08 15:40:16 +0700

    Nothing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <RouterView />
+  <!-- <RouterView /> -->
+   <Admin/>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import Admin from './components/Admin/admin.vue';
 </script>
