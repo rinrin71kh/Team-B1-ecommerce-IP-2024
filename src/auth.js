19f99af39fcfa4c3f18d3d7ace0e097a4aa48dba
Your Name <you@example.com>
2025-01-04 13:33:52 +0700

    Finish logic add to cart and login
    *Can login and signup(need validation before submit)
    *Can add to cart and minus or add more to cart sorted by status
    *Simplify code and reduce some api code

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,15 @@
 // src/auth.js
 export const AuthService = {
-    isAuthenticated: !!localStorage.getItem('authToken'),
-    login(token) {
-      localStorage.setItem('authToken', token);
-      this.isAuthenticated = true;
-    },
-    logout() {
-      localStorage.removeItem('authToken');
-      this.isAuthenticated = false;
-    },
-    isLoggedIn() {
-      return !!localStorage.getItem('authToken');
-    },
-  };
-  
+  isAuthenticated: !!localStorage.getItem('authToken'),
+  login(token) {
+    localStorage.setItem('authToken', token);
+    this.isAuthenticated = true;
+  },
+  logout() {
+    localStorage.removeItem('authToken');
+    this.isAuthenticated = false;
+  },
+  isLoggedIn() {
+    return !!localStorage.getItem('authToken');
+  },
+};
