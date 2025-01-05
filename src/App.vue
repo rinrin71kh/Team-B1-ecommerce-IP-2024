16a8bf45397af163be605d6367eb1a2dbf10d5a1
bath <Khemsombath123@gmail.com>
2025-01-05 11:36:38 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="bg-slate-100">
-    <!-- <test/> -->
+
     <MyNav/>
     <RouterView class="h-screen"/>
      <Footer/>
@@ -12,7 +12,7 @@
 import { RouterView } from 'vue-router';
 import MyNav from './components/MyNav.vue';
 import Footer from './components/Footer.vue';
-import test from './components/test.vue'; // Corrected import statement
+
 </script>
 
 
